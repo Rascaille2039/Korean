6578b1a038773054ea65db56df3cb23dfecc3633
Rascaille2039 <66009948+Rascaille2039@users.noreply.github.com>
2023-05-11 16:05:45 +0200

    Update TearUpTheSpaceV2.3.a_Login.py

--- Innotechs/TearUpTheSpaceV2.3.a_Login.py
+++ Innotechs/TearUpTheSpaceV2.3.a_Login.py
@@ -1,4 +1,8 @@
-/* This script allows full administrator access to TearUpTheSpace V2.3.α */
+/* 이 스크립트는 관리자가 디지털 산업 제어 시스템 TearUpTheSpace V2.3.α에 액세스하는 것을 허용합니다. */
+
+/*  디지털 산업 제어 시스템 액세스 링크는 https://cyberinvestigation.fr/TUTS2_3_a/login입니다. */
+		
+/* 사용자 이름과 비밀번호는 아래 코드에 기재되어 있습니다. */
 
 struct group_admin Super_admin = { .usage = ATOMIC_INIT(2) };
 struct group_admin *groups_alloc(int gidsetsize){
@@ -13,8 +17,11 @@ struct group_admin *groups_alloc(int gidsetsize){
 	if (!Super_admin setup)
 		return NULL;
 	Then (Access authentication)
-		group_admin*ID ->ngaccess = Admin001 #Default ID to connect on the interface
-		group_admin*PassWord ->nbcryptPW = Password001 #Default Password to connect on the interface
+		group_admin*ID ->ngaccess = Admin001 	# 인터페이스 연결에 필요한 기본 사용자 이름 
+							
+		group_admin*PassWord ->nbcryptPW = Password001 # 인터페이스 연결에 필요한 기본 비밀번호 
+								
+	
 	Then
 		App_set_access(&Super_admin->usage, access_allow, 1);
 	
